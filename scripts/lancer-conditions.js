0e84d0fc9a7467db49a8f4cf1f07960045e1792b
Eranziel <eranziel@gmail.com>
2020-03-30 04:06:17 +0000

    Initial commit

---
+++ scripts/lancer-conditions.js
@@ -0,0 +1,49 @@
+/**
+ * An array of status effect icons which can be applied to Tokens
+ * @type {Array}
+ */
+CONFIG.statusEffects = [
+  "modules/lancer-conditions/icons/burn.png",
+  "modules/lancer-conditions/icons/dangerzonepng",
+  "modules/lancer-conditions/icons/downandout.png",
+  "modules/lancer-conditions/icons/engaged.png",
+  "modules/lancer-conditions/icons/exposed.png",
+  "modules/lancer-conditions/icons/flying.png",
+  "modules/lancer-conditions/icons/hidden.png",
+  "modules/lancer-conditions/icons/immobilized.svg",
+  "modules/lancer-conditions/icons/impaired.png",
+  "modules/lancer-conditions/icons/jammed.svg",
+  "modules/lancer-conditions/icons/lockon.png",
+  "modules/lancer-conditions/icons/prone.png",
+  "modules/lancer-conditions/icons/shredded.svg",
+  "modules/lancer-conditions/icons/shutdown.png",
+  "modules/lancer-conditions/icons/slowed.png",
+  "modules/lancer-conditions/icons/stunned.png"
+];
+  
+// Condition Types
+// CONFIG.conditionTypes = {
+//   "blinded": "Blinded",
+//   "charmed": "Charmed",
+//   "dead": "Dead",
+//   "deafened": "Deafened",
+//   "diseased": "Diseased",
+//   "exhaustion": "Exhaustion",
+//   "exhaustion1": "Exhaustion Level 1",
+//   "exhaustion2": "Exhaustion Level 2",
+//   "exhaustion3": "Exhaustion Level 3",
+//   "exhaustion4": "Exhaustion Level 4",
+//   "exhaustion5": "Exhaustion Level 5",
+//   "frightened": "Frightened",
+//   "grappled": "Grappled",
+//   "incapacitated": "Inacapacitated",
+//   "invisible": "Invisible",
+//   "paralyzed": "Paralyzed",
+//   "petrified": "Petrified",
+//   "poisoned": "Poisoned",
+//   "prone": "Prone",
+//   "restrained": "Restrained",
+//   "stunned": "Stunned",
+//   "unconscious": "Unconscious",
+//   "wounded": "Wounded"
+// };
