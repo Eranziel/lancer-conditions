26a83441b3d24094555bc5cb5e6cec75ede33525
Eranziel <eranziel@gmail.com>
2020-06-05 05:15:48 +0000

    Define icon lists

---
+++ scripts/icons.js
@@ -0,0 +1,98 @@
+
+export const cancerIcons = [
+  "modules/lancer-conditions/icons/cancermantis/burn.png",
+  "modules/lancer-conditions/icons/cancermantis/dangerzone.png",
+  "modules/lancer-conditions/icons/cancermantis/downandout.png",
+  "modules/lancer-conditions/icons/cancermantis/engaged.png",
+  "modules/lancer-conditions/icons/cancermantis/exposed.png",
+  "modules/lancer-conditions/icons/cancermantis/flying.png",
+  "modules/lancer-conditions/icons/cancermantis/hidden.png",
+  "modules/lancer-conditions/icons/cancermantis/immobilized.svg",
+  "modules/lancer-conditions/icons/cancermantis/impaired.png",
+  "modules/lancer-conditions/icons/cancermantis/jammed.svg",
+  "modules/lancer-conditions/icons/cancermantis/lockon.png",
+  "modules/lancer-conditions/icons/cancermantis/prone.png",
+  "modules/lancer-conditions/icons/cancermantis/shredded.svg",
+  "modules/lancer-conditions/icons/cancermantis/shutdown.png",
+  "modules/lancer-conditions/icons/cancermantis/slowed.png",
+  "modules/lancer-conditions/icons/cancermantis/stunned.png"
+];
+
+export const condStatIcons = [
+  "modules/lancer-conditions/icons/condstat/bolster.png",
+  "modules/lancer-conditions/icons/condstat/downandout.png",
+  "modules/lancer-conditions/icons/condstat/exposed.png",
+  "modules/lancer-conditions/icons/condstat/grappled.png",
+  "modules/lancer-conditions/icons/condstat/hidden.png",
+  "modules/lancer-conditions/icons/condstat/immobilized.png",
+  "modules/lancer-conditions/icons/condstat/impaired.png",
+  "modules/lancer-conditions/icons/condstat/invisible.png",
+  "modules/lancer-conditions/icons/condstat/jammed.png",
+  "modules/lancer-conditions/icons/condstat/lockon.png",
+  "modules/lancer-conditions/icons/condstat/prone.png",
+  "modules/lancer-conditions/icons/condstat/shredded.png",
+  "modules/lancer-conditions/icons/condstat/shutdown.png",
+  "modules/lancer-conditions/icons/condstat/slowed.png",
+  "modules/lancer-conditions/icons/condstat/stunned.png"
+];
+
+export const pcIcons = [
+  "modules/lancer-conditions/icons/pc/aceso.png",
+  "modules/lancer-conditions/icons/pc/camus-razor.png",
+  "modules/lancer-conditions/icons/pc/chains-of-prometheus.png",
+  "modules/lancer-conditions/icons/pc/clamp-bomb.png",
+  "modules/lancer-conditions/icons/pc/dimensional-shackles.png",
+  "modules/lancer-conditions/icons/pc/dominions-breadth.png",
+  "modules/lancer-conditions/icons/pc/duat-gate.png",
+  "modules/lancer-conditions/icons/pc/excommunicate.png",
+  "modules/lancer-conditions/icons/pc/fade-cloak.png",
+  "modules/lancer-conditions/icons/pc/flaw-minus.png",
+  "modules/lancer-conditions/icons/pc/flaw-plus.png",
+  "modules/lancer-conditions/icons/pc/gravity.png",
+  "modules/lancer-conditions/icons/pc/haste.png",
+  "modules/lancer-conditions/icons/pc/hunter-lock.png",
+  "modules/lancer-conditions/icons/pc/hyperdense-armor.png",
+  "modules/lancer-conditions/icons/pc/imperial-eye.png",
+  "modules/lancer-conditions/icons/pc/kraul-grapple.png",
+  "modules/lancer-conditions/icons/pc/metahook.png",
+  "modules/lancer-conditions/icons/pc/molten-puncture.png",
+  "modules/lancer-conditions/icons/pc/retort-loop.png",
+  "modules/lancer-conditions/icons/pc/shahnameh.png",
+  "modules/lancer-conditions/icons/pc/stasis.png",
+  "modules/lancer-conditions/icons/pc/supercharger.png",
+  "modules/lancer-conditions/icons/pc/sympathetic-shield.png",
+  "modules/lancer-conditions/icons/pc/tachyon-shield.png",
+  "modules/lancer-conditions/icons/pc/terrify.png",
+  "modules/lancer-conditions/icons/pc/tracking-bug.png",
+  "modules/lancer-conditions/icons/pc/trueblack.png",
+  "modules/lancer-conditions/icons/pc/unravel.png",
+  "modules/lancer-conditions/icons/pc/viral-logic.png",
+  "modules/lancer-conditions/icons/pc/walk-of-kings.png"
+];
+
+export const npcIcons = [
+  "modules/lancer-conditions/icons/npc/abjure.png",
+  "modules/lancer-conditions/icons/npc/chain.png",
+  "modules/lancer-conditions/icons/npc/echo-edge.png",
+  "modules/lancer-conditions/icons/npc/focus-down.png",
+  "modules/lancer-conditions/icons/npc/follower-count.png",
+  "modules/lancer-conditions/icons/npc/grind-maniple.png",
+  "modules/lancer-conditions/icons/npc/illusionary-subroutines.png",
+  "modules/lancer-conditions/icons/npc/investiture.png",
+  "modules/lancer-conditions/icons/npc/latch-drone.png",
+  "modules/lancer-conditions/icons/npc/marked.png",
+  "modules/lancer-conditions/icons/npc/pain-transference.png",
+  "modules/lancer-conditions/icons/npc/petrify.png",
+  "modules/lancer-conditions/icons/npc/sanctuary.png",
+  "modules/lancer-conditions/icons/npc/spike.png",
+  "modules/lancer-conditions/icons/npc/tear-down.png",
+  "modules/lancer-conditions/icons/npc/warp-sensors.png"
+];
+
+export const utilIcons = [
+  "modules/lancer-conditions/icons/util/blind.png",
+  "modules/lancer-conditions/icons/util/burn.png",
+  "modules/lancer-conditions/icons/util/flying.png",
+  "modules/lancer-conditions/icons/util/overshield.png",
+  "modules/lancer-conditions/icons/util/reactor-meltdown.png"
+];
